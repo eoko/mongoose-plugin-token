d70a483912cfcc1f7cf8d3aa2de8af0c70cb9dbb
merlin <romain.dary@eoko.fr>
2017-05-10 00:05:06 +0200

    init repository

---
+++ plugin.js
@@ -0,0 +1,30 @@
+const randomToken = require('random-token');
+
+module.exports = function tokenPlugin(schema, opt) {
+  const defaultOptions = {
+    fieldName: 'token',
+    createIndex: true,
+    tokenLength: 64,
+  };
+
+  const options       = opt || {};
+  options.fieldName   = options.fieldName || defaultOptions.fieldName;
+  options.createIndex = options.createIndex || defaultOptions.createIndex;
+  options.tokenLength = options.tokenLength || defaultOptions.tokenLength;
+
+  const tokenSchema              = {};
+  tokenSchema[options.fieldName] = { type: String };
+
+  schema.add(tokenSchema);
+
+  if (options.createIndex) {
+    schema
+      .path(options.fieldName)
+      .index({ unique: true });
+  }
+
+  schema.pre('save', function save(next) {
+    if (this.isNew) this.token = randomToken(options.tokenLength);
+    next();
+  });
+};
